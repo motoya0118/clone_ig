9accb16a06878390a7f4ddbb894025f62f3039eb
motoya <motoyao0118@gmail.com>
2022-07-07 21:49:48 +0900

    Add UserRefToPictures_Relation

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -26,6 +26,7 @@ class PicturesController < ApplicationController
   # POST /pictures or /pictures.json
   def create
     @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id
 
     respond_to do |format|
       if @picture.save
@@ -63,6 +64,7 @@ class PicturesController < ApplicationController
 
   def confirm
     @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id
   end
   private
     # Use callbacks to share common setup or constraints between actions.
