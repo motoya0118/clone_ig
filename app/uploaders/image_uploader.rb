561ccef253b5ddb4b7f668f0dc1f3db5b7be173a
motoya <motoyao0118@gmail.com>
2022-08-12 15:50:26 +0900

    fix AWS

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   include CarrierWave::MiniMagick
   process resize_to_limit: [100, 100] # 画像サイズの調整
   # storage :fog
